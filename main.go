b012effa9b728a6d07d52cf21b31849c79f68403
Belendia <belendia@gmail.com>
2024-07-08 21:18:00 +0300

    Initial fiber setup

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	app := fiber.New()
+
+	log.Fatal(app.Listen(":4000"))
 }
