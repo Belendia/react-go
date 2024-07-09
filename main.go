2a409a8bfb4a69ab2deee63e68d68cb2927c8451
Belendia <belendia@gmail.com>
2024-07-09 08:03:51 +0300

    Add the first endpoint

--- main.go
+++ main.go
@@ -12,5 +12,9 @@ func main() {
 
 	app := fiber.New()
 
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello World"})
+	})
+
 	log.Fatal(app.Listen(":4000"))
 }
